8b501ba962916a16315a3d0231845abe437eaff9
Your Name <joaohenrique2@hotmail.com>
2023-12-28 00:58:38 -0300

    Projeto Fundamentos java  -  Entendeno o conceito dos packages

---
+++ README.md
@@ -0,0 +1,1 @@
+Este repositório foi criado com o propósito de armazenar e organizar estudos relacionados aos fundamentos da linguagem de programação Java.
---
+++ tw-java-fundamentos/src/br/com/tw_java/main/Main.java
@@ -0,0 +1,10 @@
+package br.com.tw_java.main;
+
+import java.util.Scanner;
+
+// Declaração de uma classe chamada Main
+public class Main {
+    // Método principal, ponto de entrada para a execução do programa
+    public static void main(String[] args) {
+    }
+}
